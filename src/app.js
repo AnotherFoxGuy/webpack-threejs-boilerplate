d672b3b183f32996b21f565a52034c50a53a6caf
Edgar <Edgar@AnotherFoxGuy.com>
2019-09-20 13:11:32 +0200

    :bug: Fixed eslint

--- src/app.js
+++ src/app.js
@@ -7,32 +7,32 @@ import {
   PerspectiveCamera,
   Scene,
   MeshBasicMaterial
-} from 'three'
+} from 'three';
 
 function main () {
-  const canvas = document.querySelector('#c')
-  const renderer = new WebGLRenderer({ canvas })
+  const canvas = document.querySelector('#c');
+  const renderer = new WebGLRenderer({ canvas });
 
-  const fov = 75
-  const aspect = 2 // the canvas default
-  const near = 0.1
-  const far = 5
-  const camera = new PerspectiveCamera(fov, aspect, near, far)
-  camera.position.z = 2
+  const fov = 75;
+  const aspect = 2; // the canvas default
+  const near = 0.1;
+  const far = 5;
+  const camera = new PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
 
-  const scene = new Scene()
+  const scene = new Scene();
 
-  const boxWidth = 1
-  const boxHeight = 1
-  const boxDepth = 1
-  const geometry = new BoxGeometry(boxWidth, boxHeight, boxDepth)
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-  const material = new MeshBasicMaterial({ color: 0x44aa88 }) // greenish blue
+  const material = new MeshBasicMaterial({ color: 0x44aa88 }); // greenish blue
 
-  const cube = new Mesh(geometry, material)
-  scene.add(cube)
+  const cube = new Mesh(geometry, material);
+  scene.add(cube);
 
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
 }
 
-main()
+main();
